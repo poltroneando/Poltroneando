7c762613b8b38af78320baf1ad230a963242d57b
Poltroneando <poltroneando@gmail.com>
2016-05-19 00:31:21 -0300

    versão 0.5.2

--- application/controllers/IndexController.php
+++ application/controllers/IndexController.php
@@ -3,19 +3,20 @@
 class IndexController extends Zend_Controller_Action {
 
     public function init() {
-
-        }
+        
+    }
 
     public function indexAction() {
-        if ( Zend_Auth::getInstance()->hasIdentity() ) {
-        $usuario = Zend_Auth::getInstance()->getIdentity();
-        $this->view->logado = 1;
-	$this->view->usuario = $usuario;
-        }  else {
-	$this->view->usuario = NULL ;
-        $this->view->logado = 0;
+        if (Zend_Auth::getInstance()->hasIdentity()) {
+            $usuario = Zend_Auth::getInstance()->getIdentity();
+            $this->view->logado = 1;
+            $this->view->usuario = $usuario;
+        } else {
+            $this->view->usuario = NULL;
+            $this->view->logado = 0;
 //            $usuario = 0;
         }
 //        $usuario = Zend_Auth::getInstance()->getIdentity();
     }
+
 }
